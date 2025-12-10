e9d6f82988c0c30e96e54ca7d4833908adc7871c
unknown <buariadebola@gmail.com>
2025-12-10 10:13:29 +0100

    Update backend files

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("MongoDB Error:", err));
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+    'https://ebookhub-byrenova.vercel.app/',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" })); // allows base64 image
 app.use(express.urlencoded({ extended: true }));
 
